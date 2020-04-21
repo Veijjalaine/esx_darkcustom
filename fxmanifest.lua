e6637f2a0d9f8b93f6160146046020a69e64ac59
Veijjalaine <64101685+Veijjalaine@users.noreply.github.com>
2020-04-21 22:21:09 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Likainen tuunaamo'
+
+version '2.1.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/fi.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/fi.lua',
+	'config.lua',
+	'client/main.lua'
+}
